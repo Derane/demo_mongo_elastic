57e0a1c535ec236daa433273f9f82580f729f043
Valerii Sierhieiev <vsierhieiev-its24@lll.kpi.ua>
2023-02-28 23:48:50 +0200

    Initial commit

---
+++ src/main/java/com/example/demo_mongo_elastic/DemoMongoElasticApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_mongo_elastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMongoElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMongoElasticApplication.class, args);
+	}
+
+}
